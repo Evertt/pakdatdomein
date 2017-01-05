11449edbab257dd7d573dd0b47d1ef529dbcb882
Evert van Brussel <ecvanbrussel@gmail.com>
2017-01-05 22:37:28 +0100

    rename package and add test file

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "GrijpDatDomein",
+    name: "pakdatdomein",
     dependencies: [
         .Package(url: "https://github.com/vapor/json.git", majorVersion: 1),
         .Package(url: "https://github.com/Evertt/Timepiece.git", majorVersion: 1)
